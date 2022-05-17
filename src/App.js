d254edebe7faf35fa63214bce9f019e9d9b0f7d2
Aiden McNaughton <aiden-99@hotmail.co.uk>
2022-05-17 15:57:13 +0100

    Import SignIn and ThemeProvider components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import SignIn from "./views/SignIn";
+import theme from "./theme/Theme";
+import { ThemeProvider } from "@mui/material/styles";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <SignIn />
+    </ThemeProvider>
   );
 }
 
