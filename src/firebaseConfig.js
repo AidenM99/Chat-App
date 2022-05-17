a77b84c7c80d1022b01e2de95e95f8dae25c1cdd
Aiden McNaughton <aiden-99@hotmail.co.uk>
2022-05-17 15:55:35 +0100

    Add firebaseConfig file

---
+++ src/firebaseConfig.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBZVMliQyxMD-zScpxdZ-VBEBtoJsBVcBU",
+  authDomain: "chat-app-4cf40.firebaseapp.com",
+  projectId: "chat-app-4cf40",
+  storageBucket: "chat-app-4cf40.appspot.com",
+  messagingSenderId: "613192483730",
+  appId: "1:613192483730:web:0e229bcd8ebdb1cfee374b",
+  measurementId: "G-Y1EHHLQPJN",
+};
+
+const app = initializeApp(firebaseConfig);
