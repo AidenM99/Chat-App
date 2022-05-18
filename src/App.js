0259c708b1c46afa9732e091836b98e2699f9d22
Aiden McNaughton <aiden-99@hotmail.co.uk>
2022-05-18 15:44:43 +0100

    Add conditional rendering based on authentication state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import SignIn from "./views/SignIn";
 import theme from "./theme/Theme";
+import Loading from "./views/Loading";
+import Dashboard from "./views/Dashboard";
 import { ThemeProvider } from "@mui/material/styles";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { getAuth } from "firebase/auth";
 
 function App() {
-  return (
-    <ThemeProvider theme={theme}>
-      <SignIn />
-    </ThemeProvider>
-  );
+  const [user, loading] = useAuthState(getAuth());
+
+  if (loading) {
+    return (
+      <ThemeProvider theme={theme}>
+        <Loading />
+      </ThemeProvider>
+    );
+  }
+  if (!user) {
+    return (
+      <ThemeProvider theme={theme}>
+        <SignIn />
+      </ThemeProvider>
+    );
+  }
+  if (user) {
+    return (
+      <ThemeProvider theme={theme}>
+        <Dashboard />
+      </ThemeProvider>
+    );
+  }
 }
 
 export default App;
