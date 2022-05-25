e10cf7d201ee378fa503d96091127ece013d16fe
Aiden McNaughton <aiden-99@hotmail.co.uk>
2022-05-25 16:53:57 +0100

    Add loadChats function to retrieve chats that include the current user

--- src/components/sidebar/chatsList/ChatsList.js
+++ src/components/sidebar/chatsList/ChatsList.js
@@ -1,11 +1,38 @@
 import ChatsData from "./ChatsData";
 import { StyledChatsList } from "./styles";
+import { useState, useEffect } from "react";
+import { collection, query, onSnapshot, where } from "firebase/firestore";
+import { db } from "../../../firebase";
+import { getAuth } from "firebase/auth";
 
-const ChatsList = ({ chatsList }) => {
+const ChatsList = () => {
+  const [chatsList, setChatsList] = useState([]);
+
+  const loadChats = async () => {
+    const chatsQuery = query(
+      collection(db, "chats"),
+      where(`members.${getAuth().currentUser.uid}.inChat`, "==", true)
+    );
+
+    onSnapshot(chatsQuery, (snapshot) => {
+      const chats = [];
+
+      snapshot.forEach((chat) => {
+        chats.push({ id: chat.id, data: chat.data() });
+      });
+
+      setChatsList(chats);
+    });
+  };
+
+  useEffect(() => {
+    loadChats();
+  }, []);
+
   return (
     <StyledChatsList>
-      {chatsList.map((data) => (
-        <ChatsData data={data} key={data.uid} />
+      {chatsList.map((chatData, index) => (
+        <ChatsData key={index} chatData={chatData} />
       ))}
     </StyledChatsList>
   );
