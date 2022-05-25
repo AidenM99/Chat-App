5e580623feeedd06e0b7a6aac41ba51c6e4919f7
Aiden McNaughton <aiden-99@hotmail.co.uk>
2022-05-25 16:44:37 +0100

    Import React Router components and set path on Sidebar and Chat components

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -1,20 +1,28 @@
+import Chat from "../components/chat/Chat";
 import Sidebar from "../components/sidebar/Sidebar";
 import { Box } from "@mui/material";
-import { getAuth } from "firebase/auth";
+import { Route, Routes } from "react-router-dom";
 
 const Dashboard = () => {
-  const userDetails = getAuth().currentUser;
-
   return (
     <Box
-      alignItems="center"
       backgroundColor="bgPrimary.main"
       display="flex"
       height="100vh"
       width="100vw"
     >
-      <Sidebar userDetails={userDetails} />
-      <Box flex="1" height="100vh"></Box>
+      <Routes>
+        <Route path={"/"} element={<Sidebar />} />
+        <Route
+          path={"/:chatId"}
+          element={
+            <>
+              <Sidebar />
+              <Chat />
+            </>
+          }
+        />
+      </Routes>
     </Box>
   );
 };
