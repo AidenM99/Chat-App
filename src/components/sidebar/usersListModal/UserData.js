9bc755f87e81d257dda56daa5cb4c8d1ce07ec0e
Aiden McNaughton <aiden-99@hotmail.co.uk>
2022-05-30 14:41:00 +0100

    Change variable names and reorder import statements

--- src/components/sidebar/usersListModal/UserData.js
+++ src/components/sidebar/usersListModal/UserData.js
@@ -1,6 +1,6 @@
-import { Avatar, ListItem, ListItemAvatar, ListItemText } from "@mui/material";
-import { getAuth } from "firebase/auth";
 import { db } from "../../../firebase";
+import { getAuth } from "firebase/auth";
+import { Avatar, ListItem, ListItemAvatar, ListItemText } from "@mui/material";
 import {
   addDoc,
   arrayUnion,
@@ -23,23 +23,23 @@ const UserData = ({ userData }) => {
   };
 
   const isExistingChat = async (userData) => {
-    const chatsCollectionRef = collection(db, "chats");
+    const chatsColRef = collection(db, "chats");
 
     const chatsQuery = query(
-      chatsCollectionRef,
+      chatsColRef,
       where(`members.${getAuth().currentUser.uid}.inChat`, "==", true),
       where(`members.${userData.uid}.inChat`, "==", true)
     );
 
-    const chatsQuerySnapshot = await getDocs(chatsQuery);
+    const chatsSnap = await getDocs(chatsQuery);
 
-    if (chatsQuerySnapshot.empty) return false;
+    if (chatsSnap.empty) return false;
     return true;
   };
 
   const addNewPrivateChat = async (userData) => {
     if (!(await isExistingChat(userData))) {
-      const chatDocRef = await addDoc(collection(db, "chats"), {
+      const chatsDocRef = await addDoc(collection(db, "chats"), {
         members: {
           [getAuth().currentUser.uid]: {
             inChat: true,
@@ -57,7 +57,7 @@ const UserData = ({ userData }) => {
         createdAt: serverTimestamp(),
       });
 
-      updateChats(chatDocRef.id);
+      updateChats(chatsDocRef.id);
     }
   };
 
