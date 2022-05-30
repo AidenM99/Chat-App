5999a978db5556ea7ff29f8911c2dae132298e19
Aiden McNaughton <aiden-99@hotmail.co.uk>
2022-05-30 14:44:25 +0100

    Refactor subscribe chats function and unmount the listener when component is no longer in use

--- src/components/sidebar/chatsList/ChatsList.js
+++ src/components/sidebar/chatsList/ChatsList.js
@@ -1,32 +1,34 @@
 import ChatsData from "./ChatsData";
+import { db } from "../../../firebase";
+import { getAuth } from "firebase/auth";
 import { StyledChatsList } from "./styles";
 import { useState, useEffect } from "react";
 import { collection, query, onSnapshot, where } from "firebase/firestore";
-import { db } from "../../../firebase";
-import { getAuth } from "firebase/auth";
 
 const ChatsList = () => {
   const [chatsList, setChatsList] = useState([]);
 
-  const loadChats = async () => {
+  const subscribeChats = () => {
     const chatsQuery = query(
       collection(db, "chats"),
       where(`members.${getAuth().currentUser.uid}.inChat`, "==", true)
     );
 
-    onSnapshot(chatsQuery, (snapshot) => {
-      const chats = [];
-
-      snapshot.forEach((chat) => {
-        chats.push({ id: chat.id, data: chat.data() });
-      });
-
-      setChatsList(chats);
+    return onSnapshot(chatsQuery, (snapshot) => {
+      setChatsList(
+        snapshot.docs.map((doc) => {
+          return { id: doc.id, data: doc.data() };
+        })
+      );
     });
   };
 
   useEffect(() => {
-    loadChats();
+    const unsubscribeChats = subscribeChats();
+
+    return () => {
+      unsubscribeChats();
+    };
   }, []);
 
   return (
