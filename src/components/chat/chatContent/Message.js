4b57ab4a6d7aaf0e789e93b472e6d3cc8b9d14de
Aiden McNaughton <aiden-99@hotmail.co.uk>
2022-05-31 15:25:21 +0100

    Message component now conditionally renders text or an image depending on the message contents

--- src/components/chat/chatContent/Message.js
+++ src/components/chat/chatContent/Message.js
@@ -1,15 +1,25 @@
 import { Avatar, Box, Typography } from "@mui/material";
-import { StyledMessageContainer } from "./styles";
+import { StyledMessageContainer, StyledImage } from "./styles";
 
 const Message = ({ message }) => {
   return (
     <StyledMessageContainer sx={{ p: 2 }}>
-      <Box sx={{ mr: 1.5 }}>
+      <Box sx={{ mr: 2 }}>
         <Avatar alt="profile-picture" src={message.profilePicture}></Avatar>
       </Box>
-      <Box display="flex" flexDirection="column" width="95%" sx={{ mt: -0.35 }}>
-        <Typography>{message.sentBy}</Typography>
-        <Typography>{message.messageText}</Typography>
+      <Box width="95%" sx={{ mt: -0.35 }}>
+        <Typography fontWeight="500">{message.sentBy}</Typography>
+        {message.messageText ? (
+          <Typography color="lowLight.main">{message.messageText}</Typography>
+        ) : (
+          <Box maxHeight="350px" maxWidth="350px">
+            <StyledImage
+              component="img"
+              src={message.imageURL}
+              alt="image"
+            ></StyledImage>
+          </Box>
+        )}
       </Box>
     </StyledMessageContainer>
   );
