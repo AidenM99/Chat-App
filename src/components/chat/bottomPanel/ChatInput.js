11f27fcfc3aed77a57292d29132caf4e08765aba
Aiden McNaughton <aiden-99@hotmail.co.uk>
2022-05-31 15:17:18 +0100

    Add a function to allow the user to send images and a function that updates the last message sent in the chat

--- src/components/chat/bottomPanel/ChatInput.js
+++ src/components/chat/bottomPanel/ChatInput.js
@@ -1,18 +1,41 @@
 import SendIcon from "@mui/icons-material/Send";
 import AddCircleIcon from "@mui/icons-material/AddCircle";
 import EmojiEmotionsIcon from "@mui/icons-material/EmojiEmotions";
-import { useState } from "react";
+import { useRef } from "react";
 import { db } from "../../../firebase";
 import { getAuth } from "firebase/auth";
 import { StyledTextField } from "./styles";
 import { Box, IconButton, InputAdornment } from "@mui/material";
-import { addDoc, collection, doc, serverTimestamp } from "firebase/firestore";
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "firebase/storage";
+import {
+  addDoc,
+  collection,
+  doc,
+  getDoc,
+  serverTimestamp,
+  updateDoc,
+} from "firebase/firestore";
 
-const ChatInput = ({ chatId }) => {
-  const [value, setValue] = useState();
+const ChatInput = ({
+  value,
+  chatId,
+  setValue,
+  setAnchorEl,
+  setSnackbarOpen,
+}) => {
+  const fileInput = useRef(null);
 
   const handleKeyPress = (e) => {
-    if (e.charCode === 13 && !e.shiftKey && value.trim() !== "") {
+    if (
+      (e.charCode === 13 || e.currentTarget.id === "sendMessage") &&
+      !e.shiftKey &&
+      value.trim() !== ""
+    ) {
       saveMessage();
 
       setValue("");
@@ -21,21 +44,89 @@ const ChatInput = ({ chatId }) => {
     }
   };
 
-  const saveMessage = () => {
+  const getLastMessage = (messageSnap) => {
+    if (messageSnap.data().messageText) return messageSnap.data().messageText;
+    return `${messageSnap.data().sentBy} sent an image`;
+  };
+
+  const updateLastMessage = async (chatsDocRef, chatsColRef, newMessage) => {
+    const messageRef = doc(chatsColRef, newMessage.id);
+
+    const messageSnap = await getDoc(messageRef);
+
+    await updateDoc(chatsDocRef, {
+      lastMessage: getLastMessage(messageSnap),
+    });
+  };
+
+  const saveMessage = async () => {
     const chatsDocRef = doc(db, "chats", chatId);
 
     const chatsColRef = collection(chatsDocRef, "messages");
 
-    addDoc(chatsColRef, {
+    const newMessage = await addDoc(chatsColRef, {
       messageText: value,
       sentAt: serverTimestamp(),
       sentBy: getAuth().currentUser.displayName,
       profilePicture: getAuth().currentUser.photoURL,
     });
+
+    updateLastMessage(chatsDocRef, chatsColRef, newMessage);
+  };
+
+  const saveImageMessage = async (file) => {
+    try {
+      const chatsDocRef = doc(db, "chats", chatId);
+
+      const chatsColRef = collection(chatsDocRef, "messages");
+
+      // Add a temporary image in the firestore database that will be updated later
+      const newMessage = await addDoc(chatsColRef, {
+        imageURL: "https://www.google.com/images/spin-32.gif?a",
+        sentAt: serverTimestamp(),
+        sentBy: getAuth().currentUser.displayName,
+        profilePicture: getAuth().currentUser.photoURL,
+      });
+
+      updateLastMessage(chatsDocRef, chatsColRef, newMessage);
+
+      // Add the image to cloud storage
+      const filePath = `${getAuth().currentUser.uid}/${newMessage.id}/${
+        file.name
+      }`;
+      const newImageRef = ref(getStorage(), filePath);
+      const fileSnapshot = await uploadBytesResumable(newImageRef, file);
+
+      // Generate a public URL that the temporary image in firestore will be updated with
+      const publicImageUrl = await getDownloadURL(newImageRef);
+
+      // Update the temporary image URL with the generated public image URL
+      await updateDoc(newMessage, {
+        imageURL: publicImageUrl,
+        storageUri: fileSnapshot.metadata.fullPath,
+      });
+    } catch (error) {
+      console.error(
+        "There was an error uploading a file to Cloud Storage:",
+        error
+      );
+    }
+  };
+
+  const handleChangeFile = (e) => {
+    const file = e.target.files[0];
+
+    if (!file) return;
+
+    if (!file.type.match("image.*")) {
+      setSnackbarOpen(true);
+    }
+
+    saveImageMessage(file);
   };
 
   return (
-    <Box sx={{ p: 3 }}>
+    <Box>
       <StyledTextField
         color="secondary"
         fullWidth
@@ -48,7 +139,7 @@ const ChatInput = ({ chatId }) => {
         InputProps={{
           startAdornment: (
             <InputAdornment position="start">
-              <IconButton>
+              <IconButton onClick={() => fileInput.current.click()}>
                 <AddCircleIcon color="light"></AddCircleIcon>
               </IconButton>
             </InputAdornment>
@@ -56,10 +147,10 @@ const ChatInput = ({ chatId }) => {
 
           endAdornment: (
             <InputAdornment position="end">
-              <IconButton>
+              <IconButton id="sendMessage" onClick={(e) => handleKeyPress(e)}>
                 <SendIcon color="light"></SendIcon>
               </IconButton>
-              <IconButton>
+              <IconButton onClick={(e) => setAnchorEl(e.currentTarget)}>
                 <EmojiEmotionsIcon color="light"></EmojiEmotionsIcon>
               </IconButton>
             </InputAdornment>
@@ -68,6 +159,12 @@ const ChatInput = ({ chatId }) => {
         onKeyPress={(e) => handleKeyPress(e)}
         onChange={(e) => setValue(e.target.value)}
       ></StyledTextField>
+      <input
+        ref={fileInput}
+        type="file"
+        onChange={(e) => handleChangeFile(e)}
+        style={{ display: "none" }}
+      />
     </Box>
   );
 };
