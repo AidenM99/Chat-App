08b5a5653af440d49e3dd02bbca522d55e266928
Aiden McNaughton <aiden-99@hotmail.co.uk>
2022-06-02 16:12:01 +0100

    Import ChatOptions component and adjust styling

--- src/components/chat/topPanel/RecipientInfo.js
+++ src/components/chat/topPanel/RecipientInfo.js
@@ -1,7 +1,8 @@
+import ChatOptions from "./ChatOptions";
 import { getAuth } from "firebase/auth";
 import { Avatar, Box, Typography } from "@mui/material";
 
-const RecipientInfo = ({ chatData }) => {
+const RecipientInfo = ({ chatId, chatData }) => {
   const userId = Object.keys(chatData.members).filter(
     (key) => key !== getAuth().currentUser.uid
   );
@@ -9,13 +10,18 @@ const RecipientInfo = ({ chatData }) => {
   const userInfo = chatData.members[userId];
 
   return (
-    <Box alignItems="center" display="flex">
-      <Avatar
-        alt="profile-picture"
-        src={userInfo.profilePicture}
-        sx={{ mr: 2 }}
-      />
-      <Typography fontWeight="500">{userInfo.displayName}</Typography>
+    <Box alignItems="center" display="flex" justifyContent="space-between">
+      <Box alignItems="center" display="flex">
+        <Avatar
+          alt="profile-picture"
+          src={userInfo.profilePicture}
+          sx={{ mr: 2 }}
+        />
+        <Typography fontWeight="500">{userInfo.displayName}</Typography>
+      </Box>
+      <Box>
+        <ChatOptions chatId={chatId} />
+      </Box>
     </Box>
   );
 };
