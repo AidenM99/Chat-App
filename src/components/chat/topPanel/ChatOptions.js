88fac24dc30f08607a0ce0e433b2066043adfa57
Aiden McNaughton <aiden-99@hotmail.co.uk>
2022-06-02 16:11:22 +0100

    Add ChatOptions component with delete chat functionality

---
+++ src/components/chat/topPanel/ChatOptions.js
@@ -0,0 +1,47 @@
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+import { useState } from "react";
+import { StyledMenu } from "./styles";
+import { IconButton, MenuItem } from "@mui/material";
+import { getAuth } from "firebase/auth";
+import { db } from "../../../firebase";
+import { doc, updateDoc } from "firebase/firestore";
+import { Link } from "react-router-dom";
+
+const ChatOptions = ({ chatId }) => {
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const openMenu = (e) => {
+    setAnchorEl(e.currentTarget);
+  };
+
+  const closeMenu = (e) => {
+    setAnchorEl(null);
+  };
+
+  const deleteChat = async () => {
+    const chatRef = doc(db, "chats", chatId);
+
+    await updateDoc(chatRef, {
+      [`members.${getAuth().currentUser.uid}.isHidingChat`]: true,
+    });
+  };
+
+  return (
+    <>
+      <IconButton onClick={openMenu}>
+        <MoreHorizIcon color="light" />
+      </IconButton>
+      <StyledMenu
+        anchorEl={anchorEl}
+        open={Boolean(anchorEl)}
+        onClose={closeMenu}
+      >
+        <Link to={"/"} style={{ color: "inherit", textDecoration: "none" }}>
+          <MenuItem onClick={deleteChat}>Remove Chat</MenuItem>
+        </Link>
+      </StyledMenu>
+    </>
+  );
+};
+
+export default ChatOptions;
