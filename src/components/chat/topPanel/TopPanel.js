9b08783d5f88b89eacfc09495247e466454b96ab
Aiden McNaughton <aiden-99@hotmail.co.uk>
2022-06-06 13:09:35 +0100

    Move ChatOptions to TopPanel instead of RecipientInfo

--- src/components/chat/topPanel/TopPanel.js
+++ src/components/chat/topPanel/TopPanel.js
@@ -1,15 +1,20 @@
 import RecipientInfo from "./RecipientInfo";
 import { Box } from "@mui/material";
+import ChatOptions from "./ChatOptions";
 
 const TopPanel = ({ chatId, chatData }) => {
   return (
     <Box
+      alignItems="center"
       borderBottom="1px solid rgba(255, 255, 255, 0.15)"
+      display="flex"
+      justifyContent="space-between"
       height="57px"
       width="100%"
       sx={{ px: 2, py: 1 }}
     >
-      <RecipientInfo chatId={chatId} chatData={chatData} />
+      <RecipientInfo chatData={chatData} />
+      <ChatOptions chatId={chatId} />
     </Box>
   );
 };
