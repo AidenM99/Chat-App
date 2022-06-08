aa22e6b2b40eabbcec8ac2b45b8d8f3ec46a9c0c
Aiden McNaughton <aiden-99@hotmail.co.uk>
2022-06-08 12:17:54 +0100

    Add active state toggle to user data list items

--- src/components/usersList/UserData.js
+++ src/components/usersList/UserData.js
@@ -1,8 +1,25 @@
 import { Avatar, ListItem, ListItemAvatar, ListItemText } from "@mui/material";
+import { useState } from "react";
 
 const UserData = ({ userData, userDataClickHandler }) => {
+  const [active, setActive] = useState(false);
+
+  const clickHandler = (userData) => {
+    setActive(!active);
+
+    userDataClickHandler(userData);
+  };
+
   return (
-    <ListItem button divider onClick={() => userDataClickHandler(userData)}>
+    <ListItem
+      button
+      divider
+      onClick={() => clickHandler(userData)}
+      sx={{
+        bgcolor: active && "secondary.main",
+        "&:hover": { bgcolor: active && "secondary.main" },
+      }}
+    >
       <ListItemAvatar>
         <Avatar alt="profile-picture" src={userData.photoURL}></Avatar>
       </ListItemAvatar>
